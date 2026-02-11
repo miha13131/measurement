3904b679722ed81e7eb9f582a41e738daf44b047
miha13131 <87076262+miha13131@users.noreply.github.com>
2026-02-11 14:32:15 +0100

    Delete Models/Measurement.cs

--- Models/Measurement.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DeviceMeasurementsApp.Models
-{
-    public class Measurement
-    {
-        public int DeviceId { get; set; }
-        public DateTime Timestamp { get; set; }
-        public double Value { get; set; }
-    }
-}
